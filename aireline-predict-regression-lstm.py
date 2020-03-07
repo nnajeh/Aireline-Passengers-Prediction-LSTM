f223b0a5ebc7fa4cecdf604ff810ce6e407b5754
nnajeh <38373885+nnajeh@users.noreply.github.com>
2020-03-07 15:54:06 +0100

    Update aireline-predict-regression-lstm.py

--- aireline-predict-regression-lstm.py
+++ aireline-predict-regression-lstm.py
@@ -27,52 +27,40 @@ def create_dataset(dataset, look_back=1):
     dataY.append(dataset[i + look_back, 0])
   return numpy.array(dataX), numpy.array(dataY)
 
-# split into train and test sets
+
+
+# split dataset into train and test sets
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-
+print(len(train), len(test))
+  
+  
 # reshape into X=t and Y=t+1
 look_back = 10
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
-# reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
 # create and fit the LSTM network
-def model():
-  base_model = Sequential()
-  base_model.add(LSTM(4, input_dim=look_back))
-  base_model.add(Dense(1))
-  base_model.compile(loss='mean_squared_error', optimizer='adam')
-  return base_model
-  
-model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
+model = Sequential()
+model.add(Dense(8, input_dim=look_back, activation='relu'))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
 
-# make predictions
+
+# Estimate model performance
+trainScore = model.evaluate(trainX, trainY, verbose=0)
+print( 'Train Score: %.2f MSE (%.2f RMSE)' % (trainScore, math.sqrt(trainScore)))
+testScore = model.evaluate(testX, testY, verbose=0)
+print( 'Test Score: %.2f MSE (%.2f RMSE)' % (testScore, math.sqrt(testScore)))
+
+# generate predictions for training
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 
-scaler = MinMaxScaler(feature_range=(0,1))
-dataset = scaler.fit_transform(dataset)
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
-
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
-
 # shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict# shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
@@ -82,8 +70,9 @@ testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
 testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
 
+
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
+plt.plot(dataset)
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.show()
